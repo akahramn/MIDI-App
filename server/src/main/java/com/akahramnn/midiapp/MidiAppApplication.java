42a4ab4a14677746d8d6a338f929dc901b866bac
Abdullah Kahraman <63452352+akahramn@users.noreply.github.com>
2022-04-16 19:10:45 +0300

    add reader

--- server/src/main/java/com/akahramnn/midiapp/MidiAppApplication.java
+++ server/src/main/java/com/akahramnn/midiapp/MidiAppApplication.java
@@ -1,12 +1,23 @@
 package com.akahramnn.midiapp;
 
+import Music.Setting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class MidiAppApplication {
 
 	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int instrument = in.nextInt();
+		int note = in.nextInt();
+		Setting setting = new Setting();
+		setting.play(instrument, note);
+		System.out.println(instrument + note);
 		SpringApplication.run(MidiAppApplication.class, args);
 	}
 
