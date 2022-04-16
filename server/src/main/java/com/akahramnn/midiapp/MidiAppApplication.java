8eca5fd2098ad12ba1cf4122096057dc17cb1d97
Abdullah Kahraman <63452352+akahramn@users.noreply.github.com>
2022-04-16 18:11:39 +0300

    first commit

---
+++ server/src/main/java/com/akahramnn/midiapp/MidiAppApplication.java
@@ -0,0 +1,13 @@
+package com.akahramnn.midiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidiAppApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/akahramnn/midiapp/MidiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akahramnn.midiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
